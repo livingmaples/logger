580f215787d67433f04287afb50693ef69d1b6d8
Hassan nasiri <hassan.nasiri@live.com>
2021-01-13 22:41:41 +0330

    Remove context from default events

--- event.go
+++ event.go
@@ -1,18 +1,17 @@
 package logger
 
 import (
-	"context"
 	"os"
 )
 
 type FatalEvent struct{}
 
-func (e FatalEvent) Fire(ctx context.Context, msg string, defaults map[string]interface{}) {
+func (e FatalEvent) Fire(msg string, defaults map[string]interface{}) {
 	os.Exit(1)
 }
 
 type PanicEvent struct{}
 
-func (e PanicEvent) Fire(ctx context.Context, msg string, defaults map[string]interface{}) {
+func (e PanicEvent) Fire(msg string, defaults map[string]interface{}) {
 	panic(msg)
 }
