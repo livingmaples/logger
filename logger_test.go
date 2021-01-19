a7b191e8c800adaa19b926b2d971b010fd361c4c
Hassan nasiri <hassan.nasiri@live.com>
2021-01-19 16:42:32 +0330

    rewrite test with new changes

--- logger_test.go
+++ logger_test.go
@@ -1,36 +1,46 @@
 package logger
 
 import (
-	"context"
 	"livingmaples/packages/logger/collectors"
 	"livingmaples/packages/logger/formatters"
 	"testing"
+	"time"
 )
 
 func TestJsonFormatter(t *testing.T) {
-	New(context.TODO(), &Logger{
+	New(&Logger{
 		Level: TraceLevel,
 		Defaults: map[string]interface{}{
 			"foo": "bar",
 			"moo": "goo",
 		},
-		Collector: []Collector{collectors.StderrCollector{}},
-		Formatter: formatters.JsonFormatter{},
+		Collector:  []Collector{&collectors.StderrCollector{}},
+		Formatter:  formatters.JsonFormatter{},
+		SilentMode: false,
+		Timeout:    time.Second * 3,
 	})
 
+	//l := func(i int) {
+	//	Debug("Test logging in json formatter", i)
+	//}
 	Debug("Test logging in json formatter")
+	//for i := 0; i < 200; i++ {
+	//	go l(i)
+	//}
+
+	//time.Sleep(time.Second * 5)
 }
 
-func TestTextFormatter(t *testing.T) {
-	New(context.TODO(), &Logger{
-		Level: TraceLevel,
-		Defaults: map[string]interface{}{
-			"foo": "bar",
-			"moo": "goo",
-		},
-		Collector: []Collector{collectors.StderrCollector{}},
-		Formatter: formatters.TextFormatter{},
-	})
-
-	Debug("Test logging in text formatter")
-}
+//func TestTextFormatter(t *testing.T) {
+//	New(&Logger{
+//		Level: TraceLevel,
+//		Defaults: map[string]interface{}{
+//			"foo": "bar",
+//			"moo": "goo",
+//		},
+//		Collector: []Collector{&collectors.StderrCollector{}},
+//		Formatter: formatters.TextFormatter{},
+//	})
+//
+//	Debug("Test logging in text formatter")
+//}
